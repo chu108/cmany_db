d260f43abdc76f0c5b014330a2b99fed5562e3a0
褚志勇 <zhiyong_chu@qingsongchou.com>
2020-06-28 15:49:08 +0800

    添加ETCD分布式锁

--- etcd/lock.go
+++ etcd/lock.go
@@ -2,75 +2,73 @@ package etcd
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"go.etcd.io/etcd/clientv3"
 	"sync"
 )
 
-type EtcdLock struct {
-	client    *clientv3.Client
-	ttl       int64
-	lease     clientv3.Lease
-	leaseID   clientv3.LeaseID
-	txn       clientv3.Txn
-	cancel    context.CancelFunc
-	ctx       context.Context
-	keepAlive <-chan *clientv3.LeaseKeepAliveResponse
-	lockKey   string
-	lock      sync.Mutex
-}
+var (
+	mux         sync.Mutex
+	CreateKvErr = errors.New("Failed to acquire lock")
+)
 
-//初始化锁
-func NewEtcdLock(client *clientv3.Client, ttl int64) (*EtcdLock, error) {
-	el := new(EtcdLock)
-	el.client = client
-	el.ttl = ttl
-	el.lockKey = "/cmany_db/lock"
-	//创建租约
-	el.lease = clientv3.NewLease(client)
-	leaseRes, err := el.lease.Grant(context.TODO(), ttl)
-	if err != nil {
-		return nil, err
-	}
-	el.leaseID = leaseRes.ID
-	el.ctx, el.cancel = context.WithCancel(context.TODO())
-	//抢锁和占用期间，需要不停的续租，续租方法返回一个只读的channel
-	el.keepAlive, err = el.lease.KeepAlive(el.ctx, el.leaseID)
-	if err != nil {
-		return nil, err
-	}
-	return el, nil
-}
+const lockKey = "/cmany_db/lock"
 
-//获取锁
-func (el *EtcdLock) Lock(callBack func() error) error {
-	el.lock.Lock()
-	defer el.lock.Unlock()
+func lock(client *clientv3.Client, ttl int64, callBack func() error) (err error) {
+	mux.Lock()
+	defer mux.Unlock()
+	//捕获异常
 	defer func() {
-		//两个defer用于释放锁
-		el.cancel()
-		el.lease.Revoke(el.ctx, el.leaseID)
+		if e := recover(); e != nil {
+			switch x := e.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("Unknow panic")
+			}
+		}
 	}()
+	//创建租约
+	lease := clientv3.NewLease(client)
+	leaseRes, err := lease.Grant(context.TODO(), ttl)
+	if err != nil {
+		return err
+	}
+	leaseID := leaseRes.ID
 	//创建KEY
-	kv := clientv3.NewKV(el.client)
-	el.txn = kv.Txn(context.TODO())
+	kv := clientv3.NewKV(client)
+	txn := kv.Txn(context.TODO())
 	//开始抢锁事务操作
-	el.txn.If(clientv3.Compare(clientv3.CreateRevision(el.lockKey), "=", 0)).Then(
-		clientv3.OpPut(el.lockKey, "", clientv3.WithLease(el.leaseID)),
+	txn.If(clientv3.Compare(clientv3.CreateRevision(lockKey), "=", 0)).Then(
+		clientv3.OpPut(lockKey, "", clientv3.WithLease(leaseID)),
 	).Else(
-		clientv3.OpGet(el.lockKey),
+		clientv3.OpGet(lockKey),
 	)
 	//提交事务
-	txnRes, err := el.txn.Commit()
+	txnRes, err := txn.Commit()
 	if err != nil {
 		return err
 	}
 	if txnRes.Succeeded { //抢锁成功
+		ctx, cancel := context.WithCancel(context.TODO())
+		//抢锁和占用期间，需要不停的续租，续租方法返回一个只读的channel
+		keepAlive, err := lease.KeepAlive(ctx, leaseID)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			//两个defer用于释放锁
+			cancel()
+			lease.Revoke(ctx, leaseID)
+		}()
 		//续租
 		go func() {
 			for {
 				select {
-				case leaseKeepAliveResponse := <-el.keepAlive:
+				case leaseKeepAliveResponse := <-keepAlive:
 					if leaseKeepAliveResponse == nil {
 						fmt.Println("lease fail!")
 						goto END
@@ -86,9 +84,8 @@ func (el *EtcdLock) Lock(callBack func() error) error {
 		if err != nil {
 			return err
 		}
-
 		return nil
 	} else { //抢锁成功
-		return nil
+		return CreateKvErr
 	}
 }
