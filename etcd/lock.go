450d8d5d82931cae99237db8991b1b3a91e14672
褚志勇 <zhiyong_chu@qingsongchou.com>
2020-06-28 15:50:35 +0800

    添加ETCD分布式锁

--- etcd/lock.go
+++ etcd/lock.go
@@ -15,7 +15,7 @@ var (
 
 const lockKey = "/cmany_db/lock"
 
-func lock(client *clientv3.Client, ttl int64, callBack func() error) (err error) {
+func Lock(client *clientv3.Client, ttl int64, callBack func() error) (err error) {
 	mux.Lock()
 	defer mux.Unlock()
 	//捕获异常
